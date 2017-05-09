75c649a0defeb348326f53673b3804112e38023c
butten42 <butten42@hotmail.com>
2017-05-09 19:28:58 +0800

    Update twilio.py

--- twilio.py
+++ twilio.py
@@ -1,15 +1,16 @@
+-*- coding: utf-8 -*-
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC595067541a156778084cf66e691588b2"
+account_sid = "*************"
 # Your Auth Token from twilio.com/console
-auth_token  = "b3a7d86b7672ec508b11063af82f0173"
+auth_token  = "*************"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+8613147032447", 
+    to="+****", 
     from_="+12562989011",
-    body="中文有用吗？")
+    body="脰脨***脛脫脨脫脙脗冒拢驴")
 
 print(message.sid)
